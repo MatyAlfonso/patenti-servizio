2bc05eb5ae4dc69b7d89827d846c56a36cec7048
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-03-31 14:12:07 +0200

    fix imports

--- srv/models/PatenteCivile.js
+++ srv/models/PatenteCivile.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../db.js';
+import { sequelize } from '../DataBase.js';
 
 export const PatenteCivile = sequelize.define('PatenteCivile', {
     id: {
