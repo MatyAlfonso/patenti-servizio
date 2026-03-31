227a5447253a327f520d2100aa88d03263781d3f
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-03-31 15:53:19 +0200

    fix CategoriaPatenteController

--- srv/controllers/CategoriaPatenteController.js
+++ srv/controllers/CategoriaPatenteController.js
@@ -1,10 +1,5 @@
 import { CategoriaPatente } from '../models/index.js';
 
-export const getAll = async (req, res) => {
-    try {
-        const categories = await CategoriaPatente.findAll();
-        res.json(categories);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+export const getAll = async () => {
+    return await CategoriaPatente.findAll();
 };
