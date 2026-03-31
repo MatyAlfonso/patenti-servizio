a8f747b484c1bab967ea95c74088f927c3e406e9
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-03-31 14:58:33 +0200

    add methods for Persona

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,47 @@
 const Api = {
+    // --- PERSONE ---
+
+    // GET
     async getPersone(filter) {
         return window.ipcRenderer.invoke('persone:get', filter);
     },
 
+    // CREATE
+    async createPersona(data) {
+        return window.ipcRenderer.invoke('persone:create', data);
+    },
+
+    // UPDATE
+    async updatePersona(id, data) {
+        return window.ipcRenderer.invoke('persone:update', { id, data });
+    },
+
+    // DELETE
+    async deletePersona(id) {
+        return window.ipcRenderer.invoke('persone:delete', id);
+    },
+
+    // --- ENTI ---
+
+    // GET
     async getEnti() {
         return window.ipcRenderer.invoke('enti:get');
     },
 
+    // CREATE
     async createEnte(data) {
         return window.ipcRenderer.invoke('enti:create', data);
     },
 
+    // UPDATE
     async updateEnte(id, data) {
         return window.ipcRenderer.invoke('enti:update', { id, data });
     },
 
+    // DELETE
     async deleteEnte(id) {
         return window.ipcRenderer.invoke('enti:delete', id);
     },
-
-    async get(channel, data) {
-        return window.ipcRenderer.invoke(channel, data);
-    }
 };
 
 export const apiClient = Api;
