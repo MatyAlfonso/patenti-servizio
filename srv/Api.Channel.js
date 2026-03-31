a26de3e14e934db371f15fa0d8eefa9ab3c0a2a9
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-03-31 16:14:04 +0200

    add Patenti handlers

--- srv/Api.Channel.js
+++ srv/Api.Channel.js
@@ -1,6 +1,9 @@
 import { ipcMain } from 'electron';
 import * as Persona from './controllers/PersonaController.js';
 import * as Ente from './controllers/EnteController.js';
+import * as PatenteCivile from './controllers/PatenteCivileController.js';
+import * as PatenteServizio from './controllers/PatenteServizioController.js';
+import * as Categoria from './controllers/CategoriaPatenteController.js';
 
 export const Channels = {
 	init: function () {
@@ -84,6 +87,82 @@ export const Channels = {
 				return await Ente.remove(id);
 			} catch (err) {
 				throw new Error("Impossibile eliminare l'ente: vincoli di integrità.");
+			}
+		});
+
+		// --- CATEGORIE ---
+
+		// GET
+		ipcMain.handle('categorie:get', async () => {
+			try {
+				const result = await Categoria.getAll();
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				return { error: err.message };
+			}
+		});
+
+		// --- PATENTE CIVILE ---
+
+		// GET
+		ipcMain.handle('patenteCivile:get', async () => {
+			try {
+				const result = await PatenteCivile.getAll();
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				return { error: err.message };
+			}
+		});
+
+		// CREATE
+		ipcMain.handle('patenteCivile:create', async (_, data) => {
+			try {
+				const result = await PatenteCivile.create(data);
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				throw new Error(err.message);
+			}
+		});
+
+		// UPDATE
+		ipcMain.handle('patenteCivile:update', async (_, { id, id_stato }) => {
+			try {
+				const result = await PatenteCivile.update(id, id_stato);
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				throw new Error("Errore nell'aggiornamento: " + err.message);
+			}
+		});
+
+		// --- PATENTE SERVIZIO ---
+
+		// GET
+		ipcMain.handle('patenteServizio:get', async () => {
+			try {
+				const result = await PatenteServizio.getAll();
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				return { error: err.message };
+			}
+		});
+
+		// ISSUE / CREATE FROM REQUEST
+		ipcMain.handle('patenteServizio:issue', async (_, idRichiesta) => {
+			try {
+				const result = await PatenteServizio.issue(idRichiesta);
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				throw new Error(err.message);
+			}
+		});
+
+		// UPDATE
+		ipcMain.handle('patenteServizio:update', async (_, { id, data }) => {
+			try {
+				const result = await PatenteServizio.update(id, data);
+				return JSON.parse(JSON.stringify(result));
+			} catch (err) {
+				throw new Error(err.message);
 			}
 		});
 	}
