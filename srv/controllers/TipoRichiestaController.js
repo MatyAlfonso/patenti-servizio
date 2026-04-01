47bf57dc0a4f83dac4f4253c68bbae4ed8dd1e82
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-04-01 11:36:04 +0200

    fix TipoRichiestaController

--- srv/controllers/TipoRichiestaController.js
+++ srv/controllers/TipoRichiestaController.js
@@ -1,10 +1,5 @@
 import { TipoRichiesta } from '../models/index.js';
 
-export const getAll = async (req, res) => {
-    try {
-        const categories = await TipoRichiesta.findAll();
-        res.json(categories);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+export const getAll = async () => {
+    return await TipoRichiesta.findAll();
 };
